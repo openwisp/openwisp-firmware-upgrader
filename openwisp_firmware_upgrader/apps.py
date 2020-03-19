39b72c96fb0bfa17a20a33391cbb46df80fcebf2
Federico Capoano <federico.capoano@gmail.com>
2020-03-19 16:47:13 -0500

    [admin] Added Build to side menu

--- openwisp_firmware_upgrader/apps.py
+++ openwisp_firmware_upgrader/apps.py
@@ -1,4 +1,5 @@
 from django.apps import AppConfig
+from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
 
 
@@ -6,3 +7,17 @@ class FirmwareUpdaterConfig(AppConfig):
     name = 'openwisp_firmware_upgrader'
     label = 'firmware_upgrader'
     verbose_name = _('Firmware Management')
+
+    def ready(self, *args, **kwargs):
+        self.add_default_menu_items()
+
+    def add_default_menu_items(self):
+        menu_setting = 'OPENWISP_DEFAULT_ADMIN_MENU_ITEMS'
+        items = [
+            {'model': 'firmware_upgrader.Build'},
+        ]
+        if not hasattr(settings, menu_setting):
+            setattr(settings, menu_setting, items)
+        else:
+            current_menu = getattr(settings, menu_setting)
+            current_menu += items
