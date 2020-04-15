0d7e423a9a42e0e98d751e177b9aaedaa254d489
Federico Capoano <federico.capoano@gmail.com>
2020-04-15 09:43:39 -0500

    [migrations] Removed duplication in creating default permissions #15
    Related to #15

--- openwisp_firmware_upgrader/migrations/__init__.py
+++ openwisp_firmware_upgrader/migrations/__init__.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.models import Permission
+
 from django.contrib.auth.management import create_permissions
 
 
@@ -6,3 +8,49 @@ def create_default_permissions(apps, schema_editor):
         app_config.models_module = True
         create_permissions(app_config, apps=apps, verbosity=0)
         app_config.models_module = None
+
+
+def create_permissions_for_default_groups(apps, schema_editor, app_label):
+    create_default_permissions(apps, schema_editor)
+    Group = apps.get_model('openwisp_users', 'Group')
+
+    try:
+        admin = Group.objects.get(name='Administrator')
+        operator = Group.objects.get(name='Operator')
+    # consider failures custom cases
+    # that do not have to be dealt with
+    except Group.DoesNotExist:
+        return
+
+    operators_read_only_admins_manage = [
+        'build',
+        'devicefirmware',
+        'firmwareimage',
+        'batchupgradeoperation',
+        'upgradeoperation'
+    ]
+    admins_can_manage = ['category']
+    manage_operations = ['add', 'change', 'delete']
+
+    for action in manage_operations:
+        for model_name in admins_can_manage:
+            permission = Permission.objects.get(
+                codename='{}_{}'.format(action, model_name),
+                content_type__app_label=app_label
+            )
+            admin.permissions.add(permission.pk)
+    for model_name in operators_read_only_admins_manage:
+        try:
+            permission = Permission.objects.get(
+                    codename='view_{}'.format(model_name),
+                    content_type__app_label=app_label
+            )
+            operator.permissions.add(permission.pk)
+        except Permission.DoesNotExist:
+            pass
+        for action in manage_operations:
+            permission = Permission.objects.get(
+                    codename='{}_{}'.format(action, model_name),
+                    content_type__app_label=app_label
+            )
+            admin.permissions.add(permission.pk)
