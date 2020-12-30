94288de5c970ab7fcd11a178d54bf7a45fcc98cc
Purhan <purhan01@gmail.com>
2020-12-30 23:29:14 +0530

    [qa] Added admin tests for SAMPLE_APP details field #61
    Closes #61

--- tests/openwisp2/sample_firmware_upgrader/admin.py
+++ tests/openwisp2/sample_firmware_upgrader/admin.py
@@ -2,4 +2,8 @@ from openwisp_firmware_upgrader.admin import (  # noqa
     BatchUpgradeOperationAdmin,
     BuildAdmin,
     CategoryAdmin,
+    UpgradeOperationForm,
 )
+
+BatchUpgradeOperationAdmin.fields.append('details')
+UpgradeOperationForm.Meta.fields.append('details')
--- tests/openwisp2/sample_firmware_upgrader/tests.py
+++ tests/openwisp2/sample_firmware_upgrader/tests.py
@@ -44,6 +44,67 @@ class TestAdmin(BaseTestAdmin):
     app_label = 'sample_firmware_upgrader'
     build_list_url = reverse(f'admin:{app_label}_build_changelist')
 
+    def test_category_details(self):
+        self._login()
+        category = self._create_category(details='sample category details')
+        path = reverse(f'admin:{self.app_label}_category_change', args=[category.pk])
+        r = self.client.get(path)
+        self.assertContains(
+            r, '<input type="text" name="details" value="sample category details"'
+        )
+
+    def test_build_details(self):
+        self._login()
+        build = self._create_build(details='sample build details')
+        path = reverse(f'admin:{self.app_label}_build_change', args=[build.pk])
+        r = self.client.get(path)
+        self.assertContains(
+            r, '<input type="text" name="details" value="sample build details"'
+        )
+
+    def test_firmware_image_details(self):
+        self._login()
+        build = self._create_build()
+        self._create_firmware_image(details='sample fw_image details', build=build)
+        path = reverse(f'admin:{self.app_label}_build_change', args=[build.pk])
+        r = self.client.get(path)
+        self.assertContains(r, '<div class="readonly">sample fw_image details')
+
+    def test_device_firmware_details(self):
+        self._login()
+        device_fw = self._create_device_firmware(details='sample device_fw details')
+        path = reverse('admin:config_device_change', args=[device_fw.device_id])
+        r = self.client.get(path)
+        self.assertContains(
+            r,
+            '<input type="text" name="devicefirmware-0-details" '
+            'value="sample device_fw details" class="vTextField"',
+        )
+
+    def test_batch_upgrade_operation_details(self):
+        self._login()
+        env = self._create_upgrade_env()
+        env['build1'].batch_upgrade(firmwareless=True)
+        buo = BatchUpgradeOperation.objects.first()
+        buo.details = 'Test BatchUpgrade details'
+        buo.save()
+        url = reverse(
+            f'admin:{self.app_label}_batchupgradeoperation_change', args=[buo.pk]
+        )
+        r = self.client.get(url)
+        self.assertContains(r, '<div class="readonly">Test BatchUpgrade details')
+
+    def test_upgrede_operation_details(self):
+        self._login()
+        device_fw = self._create_device_firmware()
+        device_fw.save(upgrade=True)
+        uo = UpgradeOperation.objects.first()
+        uo.details = 'Test Upgrade device details'
+        uo.save()
+        url = reverse('admin:config_device_change', args=[device_fw.device.pk])
+        r = self.client.get(url)
+        self.assertContains(r, '<div class="readonly">Test Upgrade device details')
+
 
 class TestAdminTransaction(BaseTestAdminTransaction):
     app_label = 'sample_firmware_upgrader'
