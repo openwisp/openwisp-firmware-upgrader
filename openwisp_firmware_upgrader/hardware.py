c139cf3d44ce2c58c9c9dbd01aeffccbf464ab2d
TheMiNuS <58090715+bgiboudeau@users.noreply.github.com>
2021-03-17 19:34:32 +0100

    [chores] Add images for TP-Link,  Linksys, Raspberry, VMWare
     TP-Link Archer C7 v1 to v5
             WR902AC
             CPE-210v3
             CPE-510v3
             WR2543N/ND
     Raspberry
             Pi 2
             Pi 3
     Linksys
             WRT1900ACS
             WRT3200ACM
     VMWare Virtual Appliance
             X86_64
     ZBT
             WG3526

--- openwisp_firmware_upgrader/hardware.py
+++ openwisp_firmware_upgrader/hardware.py
@@ -39,6 +39,84 @@ OPENWRT_FIRMWARE_IMAGE_MAP.update(
                 'ar71xx-generic-ubnt-airrouter-squashfs-sysupgrade.bin',
                 {'label': 'Ubiquiti AirRouter', 'boards': ('Ubiquiti AirRouter',)},
             ),
+            (
+                'ramips-mt7621-zbt-wg3526-16M-squashfs-sysupgrade.bin',
+                {'label': 'ZBT-WG3526 (16M)', 'boards': ('ZBT-WG3526 (16M)',)},
+            ),
+            (
+                'ath79-generic-tplink_tl-wr2543-v1-squashfs-sysupgrade.bin',
+                {
+                    'label': 'TP-Link TL-WR2543N/ND',
+                    'boards': ('TP-Link TL-WR2543N/ND',),
+                },
+            ),
+            (
+                'ath79-generic-tplink_cpe210-v3-squashfs-sysupgrade.bin',
+                {'label': 'TP-LINK CPE210 v3', 'boards': ('TP-LINK CPE210 v3',)},
+            ),
+            (
+                'ath79-generic-tplink_cpe510-v3-squashfs-sysupgrade.bin',
+                {'label': 'TP-LINK CPE510 v3', 'boards': ('TP-LINK CPE510 v3',)},
+            ),
+            (
+                'ath79-generic-tplink_archer-c7-v1-squashfs-sysupgrade.bin',
+                {'label': 'TP-Link Archer C7 v1', 'boards': ('tplink,archer-c7-v1',)},
+            ),
+            (
+                'ath79-generic-tplink_archer-c7-v2-squashfs-sysupgrade.bin',
+                {
+                    'label': 'TP-Link Archer C7 v2',
+                    'boards': ('TP-Link Archer C7 v2', 'TP-Link Archer C7 v3'),
+                },
+            ),
+            (
+                'ath79-generic-tplink_archer-c7-v4-squashfs-sysupgrade.bin',
+                {'label': 'TP-Link Archer C7 v4', 'boards': ('TP-Link Archer C7 v4',)},
+            ),
+            (
+                'ath79-generic-tplink_archer-c7-v5-squashfs-sysupgrade.bin',
+                {'label': 'TP-Link Archer C7 v5', 'boards': ('TP-Link Archer C7 v5',)},
+            ),
+            (
+                'ramips-mt76x8-tplink_tl-wr902ac-v3-squashfs-sysupgrade.bin',
+                {
+                    'label': 'TP-Link TL-WR902AC v3',
+                    'boards': ('TP-Link TL-WR902AC v3',),
+                },
+            ),
+            (
+                'brcm2708-bcm2710-rpi-3-ext4-sysupgrade.img.gz',
+                {
+                    'label': 'Raspberry Pi 3 Model B',
+                    'boards': ('Raspberry Pi 3 Model B Rev 1.2',),
+                },
+            ),
+            (
+                'brcm2708-bcm2709-rpi-2-ext4-sysupgrade.img.gz',
+                {
+                    'label': 'Raspberry Pi 2 Model B',
+                    'boards': (
+                        'Raspberry Pi 2 Model B Rev 1.0',
+                        'Raspberry Pi 2 Model B Rev 1.1',
+                        'Raspberry Pi 2 Model B Rev 1.2',
+                    ),
+                },
+            ),
+            (
+                'x86-64-generic-squashfs-sysupgrade.bin',
+                {
+                    'label': 'VMware, Inc. VMware Virtual Platform',
+                    'boards': ('VMware, Inc. VMware Virtual Platform',),
+                },
+            ),
+            (
+                'mvebu-cortexa9-linksys_wrt1900acs-squashfs-sysupgrade.img',
+                {'label': 'Linksys WRT1900ACS', 'boards': ('Linksys WRT1900ACS',)},
+            ),
+            (
+                'mvebu-cortexa9-linksys_wrt3200acm-squashfs-sysupgrade.img',
+                {'label': 'Linksys WRT3200ACM', 'boards': ('Linksys WRT3200ACM',)},
+            ),
         )
     )
 )
