fa6c99435830627e8886827c2f82e8b68b89b883
Ajay Tripathi <ajay39in@gmail.com>
2021-04-19 00:11:10 +0530

    [settings]  Made FIRMWARE_API_BASEURL functionationlity generic

--- openwisp_firmware_upgrader/private_storage/urls.py
+++ openwisp_firmware_upgrader/private_storage/urls.py
@@ -1,7 +1,14 @@
-from django.conf.urls import url
+from urllib.parse import urljoin
 
+from django.urls import path
+
+from ..settings import IMAGE_URL_PATH
 from . import views
 
 urlpatterns = [
-    url(r'^(?P<path>.*)$', views.firmware_image_download, name='serve_private_file'),
+    path(
+        urljoin(IMAGE_URL_PATH, '<path:imagefile>'),
+        views.firmware_image_download,
+        name='serve_private_file',
+    ),
 ]
