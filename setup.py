49ecf98bbf123b8fccfc303e14d4838c9de4129d
Gagan Deep <pandafy.dev@gmail.com>
2022-01-12 23:36:33 +0530

    [temp] Set openwisp-controller version

--- setup.py
+++ setup.py
@@ -7,7 +7,10 @@ from setuptools import find_packages, setup
 from openwisp_firmware_upgrader import get_version
 
 # TODO: change this when next version of openwisp_controller is released
-controller = 'https://github.com/openwisp/openwisp-controller/tarball/master'
+controller = (
+    'https://github.com/openwisp/openwisp-controller/tarball/'
+    'improved-deleted-device-reversion'
+)
 # TODO: change this when next version of openwisp_utils is released
 utils = 'https://github.com/openwisp/openwisp-utils/tarball/master'
 
