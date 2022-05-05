e1669998e9ad9b5ede5fc54c2be69cabc494cb40
Gagan Deep <pandafy.dev@gmail.com>
2022-05-06 03:21:47 +0530

    1.0.0 release
    Co-authored-by: Federico Capoano <f.capoano@openwisp.io>

--- openwisp_firmware_upgrader/__init__.py
+++ openwisp_firmware_upgrader/__init__.py
@@ -1,4 +1,4 @@
-VERSION = (0, 1, 1, 'final')
+VERSION = (1, 0, 0, 'final')
 __version__ = VERSION  # alias
 
 
--- setup.py
+++ setup.py
@@ -6,11 +6,6 @@ from setuptools import find_packages, setup
 
 from openwisp_firmware_upgrader import get_version
 
-# TODO: change this when next version of openwisp_controller is released
-controller = 'https://github.com/openwisp/openwisp-controller/tarball/master'
-# TODO: change this when next version of openwisp_utils is released
-utils = 'https://github.com/openwisp/openwisp-utils/tarball/master'
-
 if sys.argv[-1] == 'publish':
     # delete any *.pyc, *.pyo and __pycache__
     os.system('find . | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf')
@@ -40,8 +35,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        f'openwisp-controller @ {controller}',
-        f'openwisp-utils[rest] @ {utils}',
+        'openwisp-controller~=1.0.0',
         'django-private-storage~=3.0.0',
     ],
     classifiers=[
