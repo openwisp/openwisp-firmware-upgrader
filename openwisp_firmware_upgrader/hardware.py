ca0582016468a05575aac14f231041dc5c688da5
KoS <kosli@users.noreply.github.com>
2023-01-25 23:10:42 +0100

    PC Engines apu6 should use x86/64 firmware
    As I have tested an PC Engines apu6 board, this can be added to the list too.

--- openwisp_firmware_upgrader/hardware.py
+++ openwisp_firmware_upgrader/hardware.py
@@ -475,7 +475,7 @@ OPENWRT_FIRMWARE_IMAGE_MAP.update(
                 'x86-64-generic-squashfs-combined.img.gz',
                 {
                     'label': 'Generic x86/64 (QEMU/KVM)',
-                    'boards': ('x86_64', 'PC Engines APU2'),
+                    'boards': ('x86_64', 'PC Engines APU2', 'PC Engines apu6'),
                 },
             ),
             (
