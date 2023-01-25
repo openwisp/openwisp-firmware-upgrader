0b42d822232bb9be64a62f1a97950f43c67072c9
KoS <kosli@users.noreply.github.com>
2023-01-25 23:17:05 +0100

    PC Engines apu2 in lower case
    I have seen that the model name "apu" provided to OpenWisp is written in lower case and that the firmware wasn't matched otherwise when upgrading.

--- openwisp_firmware_upgrader/hardware.py
+++ openwisp_firmware_upgrader/hardware.py
@@ -475,7 +475,7 @@ OPENWRT_FIRMWARE_IMAGE_MAP.update(
                 'x86-64-generic-squashfs-combined.img.gz',
                 {
                     'label': 'Generic x86/64 (QEMU/KVM)',
-                    'boards': ('x86_64', 'PC Engines APU2', 'PC Engines apu6'),
+                    'boards': ('x86_64', 'PC Engines apu2', 'PC Engines apu6'),
                 },
             ),
             (
