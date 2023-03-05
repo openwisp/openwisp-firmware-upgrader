8045f79e2f22ef065dcd362630f75aa80dcfa44c
Gagan Deep <pandafy.dev@gmail.com>
2023-03-05 10:18:57 +0530

    [tests] Fixed test for SAMPLE_APP

--- tests/openwisp2/sample_firmware_upgrader/admin.py
+++ tests/openwisp2/sample_firmware_upgrader/admin.py
@@ -2,8 +2,10 @@ from openwisp_firmware_upgrader.admin import (  # noqa
     BatchUpgradeOperationAdmin,
     BuildAdmin,
     CategoryAdmin,
+    DeviceUpgradeOperationInline,
     UpgradeOperationForm,
 )
 
 BatchUpgradeOperationAdmin.fields.append('details')
 UpgradeOperationForm.Meta.fields.append('details')
+DeviceUpgradeOperationInline.fields.append('details')
