5ea979779dca1ec54bf96654dce66baf4caa622a
Aryamanz29 <aryamanz29@gmail.com>
2023-05-24 15:30:43 +0530

    [change] Switched to openwisp-utils master

--- openwisp_firmware_upgrader/tests/test_selenium.py
+++ openwisp_firmware_upgrader/tests/test_selenium.py
@@ -5,7 +5,7 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.core.management import call_command
 from django.urls.base import reverse
 from reversion.models import Version
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import TimeoutException, UnexpectedAlertPresentException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select, WebDriverWait
@@ -52,23 +52,23 @@ class TestDeviceAdmin(TestUpgraderMixin, SeleniumTestMixin, StaticLiveServerTest
             username=self.admin_username, password=self.admin_password
         )
 
-    # def tearDown(self):
-    #     # Accept unsaved changes alert to allow other tests to run
-    #     try:
-    #         self.web_driver.refresh()
-    #     except UnexpectedAlertPresentException:
-    #         self.web_driver.switch_to_alert().accept()
-    #     else:
-    #         try:
-    #             WebDriverWait(self.web_driver, 1).until(EC.alert_is_present())
-    #         except TimeoutException:
-    #             pass
-    #         else:
-    #             self.web_driver.switch_to_alert().accept()
-    #     self.web_driver.refresh()
-    #     WebDriverWait(self.web_driver, 2).until(
-    #         EC.visibility_of_element_located((By.XPATH, '//*[@id="site-name"]'))
-    #     )
+    def tearDown(self):
+        # Accept unsaved changes alert to allow other tests to run
+        try:
+            self.web_driver.refresh()
+        except UnexpectedAlertPresentException:
+            self.web_driver.switch_to_alert().accept()
+        else:
+            try:
+                WebDriverWait(self.web_driver, 1).until(EC.alert_is_present())
+            except TimeoutException:
+                pass
+            else:
+                self.web_driver.switch_to_alert().accept()
+        self.web_driver.refresh()
+        WebDriverWait(self.web_driver, 2).until(
+            EC.visibility_of_element_located((By.XPATH, '//*[@id="site-name"]'))
+        )
 
     @capture_any_output()
     def test_restoring_deleted_device(self):
