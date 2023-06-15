fb801fbe43656b1fc6c1cda0c7785d2b4233b9fb
Gagan Deep <pandafy.dev@gmail.com>
2023-06-15 17:44:23 +0530

    [deps] Added support for Django 4.1.0 and 4.2.0
    - Dropped support for Python 3.7
    - Dropped support for Django 4.0.0
    - Bumped django-private-storage~=3.1.0

--- setup.py
+++ setup.py
@@ -35,9 +35,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'openwisp-controller @ https://github.com/openwisp/openwisp-controller/tarball/master',
-        'openwisp-utils[rest] @ https://github.com/openwisp/openwisp-utils/tarball/master',
-        'django-private-storage~=3.0.0',
+        'django-private-storage~=3.1.0',
     ],
     classifiers=[
         'Development Status :: 3 - Alpha',
