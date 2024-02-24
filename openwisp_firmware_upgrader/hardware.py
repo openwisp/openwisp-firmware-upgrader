9a88601a1c27bd1da85645eb88f659e2686ae3d5
Federico Capoano <f.capoano@openwisp.io>
2024-02-24 12:36:46 -0300

    [chores] Added new label for Ubiquiti UniFi AP

--- openwisp_firmware_upgrader/hardware.py
+++ openwisp_firmware_upgrader/hardware.py
@@ -372,7 +372,7 @@ OPENWRT_FIRMWARE_IMAGE_MAP.update(
                 'ath79-generic-ubnt_unifi-squashfs-sysupgrade.bin',
                 {
                     'label': 'Ubiquiti UniFi (OpenWRT 19.07 and later)',
-                    'boards': ('Ubiquiti UniFi',),
+                    'boards': ('Ubiquiti UniFi', 'Ubiquiti UniFi AP'),
                 },
             ),
             # Ubiquiti UniFi AC LR ATH79
