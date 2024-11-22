d43614f63c057acbe25a7185c23412b83df0bef4
Gagan Deep <pandafy.dev@gmail.com>
2024-11-23 02:05:30 +0530

    [deps] Bumped openwisp-controller~=1.1.0

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'openwisp-controller @ https://github.com/openwisp/openwisp-controller/tarball/master',
+        'openwisp-controller~=1.1.0',
         'django-private-storage~=3.1.0',
     ],
     classifiers=[
