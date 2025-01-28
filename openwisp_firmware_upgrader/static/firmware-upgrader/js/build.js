af5effb32c48ecc66a5139be12c9a37e154ea975
Roshan Kumar <roshaen09@gmail.com>
2025-01-28 19:27:04 +0530

    [qa] Switched to prettier for CSS/JS formatting

--- openwisp_firmware_upgrader/static/firmware-upgrader/js/build.js
+++ openwisp_firmware_upgrader/static/firmware-upgrader/js/build.js
@@ -1,11 +1,11 @@
-'use strict';
+"use strict";
 
 django.jQuery(function ($) {
-    function initSelect2() {
-        $('select[id^="id_firmwareimage_set"][id$="type"]')
-        .not('select[name*="__prefix__"]')
-        .select2();
-    }
-    $('.add-row > a').click(initSelect2);
-    initSelect2();
+  function initSelect2() {
+    $('select[id^="id_firmwareimage_set"][id$="type"]')
+      .not('select[name*="__prefix__"]')
+      .select2();
+  }
+  $(".add-row > a").click(initSelect2);
+  initSelect2();
 });
