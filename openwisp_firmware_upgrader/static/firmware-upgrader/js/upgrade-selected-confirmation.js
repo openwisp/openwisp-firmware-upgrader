a8cf4e5d7d780f4745b6b8b54b51c8ff336c2274
Roshan Kumar <roshaen09@gmail.com>
2025-01-28 19:27:04 +0530

    [qa] Switched to prettier for CSS/JS formatting

--- openwisp_firmware_upgrader/static/firmware-upgrader/js/upgrade-selected-confirmation.js
+++ openwisp_firmware_upgrader/static/firmware-upgrader/js/upgrade-selected-confirmation.js
@@ -1,16 +1,15 @@
-'use strict';
+"use strict";
 
 django.jQuery(function ($) {
-    if(firmwareUpgraderSchema === null) {
-        $('.form-row').hide();
-
-    } else {
-        django._loadJsonSchemaUi(
-            $('textarea[name="upgrade_options"]').get(0),
-            false,
-            firmwareUpgraderSchema,
-            true
-        );
-    }
-    $('#ow-loading').hide();
+  if (firmwareUpgraderSchema === null) {
+    $(".form-row").hide();
+  } else {
+    django._loadJsonSchemaUi(
+      $('textarea[name="upgrade_options"]').get(0),
+      false,
+      firmwareUpgraderSchema,
+      true,
+    );
+  }
+  $("#ow-loading").hide();
 });
