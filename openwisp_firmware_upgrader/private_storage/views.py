18775b0639bcce1ad72ea8d81517fc30cf43fd54
Roshan Kumar <roshaen09@gmail.com>
2025-04-25 15:53:14 +0000

    [change] Improved endpoints to download firmware images

--- openwisp_firmware_upgrader/private_storage/views.py
+++ openwisp_firmware_upgrader/private_storage/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_permission_codename
 from private_storage.views import PrivateStorageDetailView
 
 from ..swapper import load_model
@@ -12,9 +13,26 @@ class FirmwareImageDownloadView(PrivateStorageDetailView):
 
     def can_access_file(self, private_file):
         user = private_file.request.user
-        return user.is_superuser or (
+        
+        # Check if user is superuser or manager of the organization
+        is_authorized = user.is_superuser or (
             user.is_staff and user.is_manager(self.object.build.category.organization)
         )
+        
+        # If user is not authorized by role, deny access
+        if not is_authorized:
+            return False
+        
+        # For authorized users, check view permission
+        # Only if they're not superusers (superusers have all permissions)
+        if not user.is_superuser:
+            perm_codename = get_permission_codename('view', self.model._meta)
+            view_perm = f'{self.model._meta.app_label}.{perm_codename}'
+            has_view_perm = user.has_perm(view_perm, self.object)
+            if not has_view_perm:
+                return False
+        
+        return True
 
 
 firmware_image_download = FirmwareImageDownloadView.as_view()
