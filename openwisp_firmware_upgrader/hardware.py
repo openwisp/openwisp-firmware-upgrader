c4ecac121db38779aef1148b022e6b1a1d188078
Federico Capoano <f.capoano@openwisp.io>
2025-06-26 20:21:51 -0300

    [chores] Updated x86 definitions

--- openwisp_firmware_upgrader/hardware.py
+++ openwisp_firmware_upgrader/hardware.py
@@ -554,14 +554,22 @@ OPENWRT_FIRMWARE_IMAGE_MAP.update(
                 },
             ),
             (
-                'x86-64-generic-squashfs-combined.img.gz',
                 {
-                    'label': 'Generic x86/64 (QEMU/KVM)',
-                    'boards': (
-                        'x86_64',
-                        'PC Engines APU2',
-                        'PC Engines apu2',
-                        'PC Engines apu6',
+                    "label": "Generic x86/64 (UEFI)",
+                    "boards": (
+                        "x86_64_efi"
+                    ),
+                },
+            ),
+            (
+                "x86-64-generic-squashfs-combined.img.gz",
+                {
+                    "label": "Generic x86/64 (BIOS)",
+                    "boards": (
+                        "x86_64",
+                        "PC Engines APU2",
+                        "PC Engines apu2",
+                        "PC Engines apu6",
                     ),
                 },
             ),
