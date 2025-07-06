71bf0cecc990b16e6471bcceeab9ce5b55eac0f7
AjibadeHassan <hassanajibade17@gmail.com>
2025-07-06 20:34:30 +0100

    retry-firmware solution

--- openwisp_firmware_upgrader/base/models.py
+++ openwisp_firmware_upgrader/base/models.py
@@ -54,7 +54,7 @@ class UpgradeOptionsMixin(models.Model):
     def validate_upgrade_options(self):
         if not self.upgrade_options:
             return
-        if not getattr(self.upgrader_class, "SCHEMA"):
+        if not getattr(self.upgrader_class, "SCHEMA", None):
             raise ValidationError(
                 _("Using upgrade options is not allowed with this upgrader.")
             )
@@ -89,34 +89,10 @@ class AbstractBuild(TimeStampedEditableModel):
         get_model_name("Category"),
         on_delete=models.CASCADE,
         verbose_name=_("firmware category"),
-        help_text=_(
-            "if you have different firmware types "
-            "eg: (BGP routers, wifi APs, DSL gateways) "
-            "create a category for each."
-        ),
     )
     version = models.CharField(max_length=32, db_index=True)
-    os = models.CharField(
-        _("OS identifier"),
-        max_length=64,
-        blank=True,
-        null=True,
-        help_text=_(
-            "OS identifier as presented by the device, "
-            "used to automatically recognize the firmware "
-            "image used by new devices that register "
-            "into the system"
-        ),
-    )
-    changelog = models.TextField(
-        _("change log"),
-        blank=True,
-        help_text=_(
-            "descriptive text indicating what "
-            "has changed since the previous "
-            "version, if applicable"
-        ),
-    )
+    os = models.CharField(_("OS identifier"), max_length=64, blank=True, null=True)
+    changelog = models.TextField(_("change log"), blank=True)
 
     class Meta:
         abstract = True
@@ -132,7 +108,6 @@ class AbstractBuild(TimeStampedEditableModel):
             return super().__str__()
 
     def clean(self):
-        # Make sure that ('category__organization', 'os') is unique too
         try:
             category = self.category
         except ObjectDoesNotExist:
@@ -167,10 +142,6 @@ class AbstractBuild(TimeStampedEditableModel):
         return batch
 
     def _find_related_device_firmwares(self, select_devices=False):
-        """
-        Returns all the DeviceFirmware objects related to the firmware
-        category of this build that have not been installed yet
-        """
         related = ["image"]
         if select_devices:
             related.append("device")
@@ -184,19 +155,12 @@ class AbstractBuild(TimeStampedEditableModel):
         )
 
     def _find_firmwareless_devices(self, boards=None):
-        """
-        Returns devices which have no related DeviceFirmware
-        but that are upgradable to one of the image of this build
-        """
         if boards is None:
             boards = []
             for image in self.firmwareimage_set.all():
                 boards += image.boards
         Device = swapper.load_model("config", "Device")
-        qs = Device.objects.filter(
-            devicefirmware__isnull=True,
-            model__in=boards,
-        )
+        qs = Device.objects.filter(devicefirmware__isnull=True, model__in=boards)
         if self.category.organization_id:
             qs = qs.filter(organization_id=self.category.organization_id)
         return qs.order_by("-created")
@@ -220,11 +184,7 @@ class AbstractFirmwareImage(TimeStampedEditableModel):
         blank=True,
         max_length=128,
         choices=FIRMWARE_IMAGE_TYPE_CHOICES,
-        help_text=_(
-            "firmware image type: model or "
-            "architecture. Leave blank to attempt "
-            "determining automatically"
-        ),
+        help_text=_("firmware image type"),
     )
 
     class Meta:
@@ -256,26 +216,18 @@ class AbstractFirmwareImage(TimeStampedEditableModel):
     def _remove_file(self):
         firmware_filename = self.file.name
         self.file.storage.delete(firmware_filename)
-        # Delete the file directory
         dir = os.path.split(firmware_filename)[0]
         if dir:
             try:
                 self.file.storage.delete(dir)
             except OSError as error:
-                # A build can have multiple firmware images,
-                # therefore, deleting directory might not be
-                # always feasible.
                 if "Directory not empty" not in str(error):
                     raise error
 
     def _clean_type(self):
-        """
-        auto determine type if missing
-        """
         if self.type:
             return
         filename = self.file.name
-        # removes leading prefix
         self.type = "-".join(filename.split("-")[1:])
 
 
@@ -287,10 +239,6 @@ class AbstractDeviceFirmware(TimeStampedEditableModel):
     installed = models.BooleanField(default=False)
     _old_image = None
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._update_old_image()
-
     class Meta:
         verbose_name = _("Device Firmware")
         abstract = True
@@ -299,23 +247,21 @@ class AbstractDeviceFirmware(TimeStampedEditableModel):
         if not hasattr(self, "image") or not hasattr(self, "device"):
             return
         if (
-            self.image.build.category.organization is not None
+            self.image.build.category.organization
             and self.image.build.category.organization != self.device.organization
         ):
             raise ValidationError(
                 {
                     "image": _(
-                        "The organization of the image doesn't "
-                        "match the organization of the device"
+                        "The organization of the image doesn't match the device"
                     )
                 }
             )
         if self.device.deviceconnection_set.count() < 1:
             raise ValidationError(
                 _(
-                    "This device does not have a related connection object defined "
-                    "yet and therefore it would not be possible to upgrade it, "
-                    'please add one in the section named "Credentials"'
+                    "This device does not have a related connection defined "
+                    "yet. Please add one in the 'Credentials' section."
                 )
             )
         if self.device.model not in self.image.boards:
@@ -326,8 +272,6 @@ class AbstractDeviceFirmware(TimeStampedEditableModel):
         return self._state.adding or self.image_id != self._old_image.id
 
     def save(self, batch=None, upgrade=True, upgrade_options=None, *args, **kwargs):
-        # if firwmare image has changed launch upgrade
-        # upgrade won't be launched the first time
         if upgrade and (self.image_has_changed or not self.installed):
             self.installed = False
             super().save(*args, **kwargs)
@@ -349,85 +293,8 @@ class AbstractDeviceFirmware(TimeStampedEditableModel):
             operation.batch = batch
         operation.full_clean()
         operation.save()
-        # launch ``upgrade_firmware`` in the background (celery)
-        # once changes are committed to the database
         transaction.on_commit(lambda: upgrade_firmware.delay(operation.pk))
         return operation
-
-    @classmethod
-    def create_for_device(cls, device, firmware_image=None):
-        """
-        Creates a ``DeviceFirmware`` instance for the specified device
-        If ``firmware_image`` is not supplied, it will be tried
-        to be determined automatically.
-
-        May return ``None`` if it was not possible to create the DeviceFirmware.
-        """
-        DeviceFirmware = load_model("DeviceFirmware")
-        FirmwareImage = load_model("FirmwareImage")
-        image_type = REVERSE_FIRMWARE_IMAGE_MAP.get(device.model)
-
-        if not image_type:
-            return
-
-        if not firmware_image:
-            try:
-                firmware_image = FirmwareImage.objects.get(
-                    build__category__organization_id=device.organization_id,
-                    build__os=device.os,
-                    type=image_type,
-                )
-            except FirmwareImage.DoesNotExist:
-                return
-
-        device_fw = DeviceFirmware(device=device, image=firmware_image, installed=True)
-        try:
-            device_fw.full_clean()
-        except ValidationError as e:
-            logger.warning(e)
-            return
-        device_fw.save(upgrade=False)
-        return device_fw
-
-    @classmethod
-    def auto_add_device_firmware_to_device(cls, instance, created, **kwargs):
-        # Automatically associate DeviceFirmware to the registered Device
-        if not created:
-            return
-        if not instance.device.os or not instance.device.model:
-            return
-        if instance.device.model not in REVERSE_FIRMWARE_IMAGE_MAP:
-            return
-
-        transaction.on_commit(lambda: create_device_firmware.delay(instance.device.pk))
-
-    @classmethod
-    def auto_create_device_firmwares(cls, instance, created, **kwargs):
-        if created:
-            transaction.on_commit(
-                lambda: create_all_device_firmwares.delay(instance.pk)
-            )
-
-    @classmethod
-    def get_image_queryset_for_device(cls, device, device_firmware=None):
-        FirmwareImage = cls.image.field.related_model
-        qs = (
-            FirmwareImage.objects.filter(
-                Q(build__category__organization_id=device.organization_id)
-                | Q(build__category__organization__isnull=True)
-            )
-            .order_by("-created")
-            .select_related("build", "build__category")
-        )
-        # if device model is defined
-        # restrict the images to the ones compatible with it
-        if device.model and device.model in REVERSE_FIRMWARE_IMAGE_MAP:
-            qs = qs.filter(type=REVERSE_FIRMWARE_IMAGE_MAP[device.model])
-        # if DeviceFirmware instance already exists
-        # restrict images to the ones of the same category
-        if device_firmware and hasattr(device_firmware, "image"):
-            qs = qs.filter(build__category_id=device_firmware.image.build.category_id)
-        return qs
 
 
 class AbstractBatchUpgradeOperation(UpgradeOptionsMixin, TimeStampedEditableModel):
@@ -454,7 +321,6 @@ class AbstractBatchUpgradeOperation(UpgradeOptionsMixin, TimeStampedEditableMode
         operations = self.upgradeoperation_set
         if operations.filter(status="in-progress").exists():
             return
-        # if there's any failed operation, mark as failure
         if operations.filter(status="failed").exists():
             self.status = "failed"
         else:
@@ -468,38 +334,16 @@ class AbstractBatchUpgradeOperation(UpgradeOptionsMixin, TimeStampedEditableMode
         if firmwareless:
             self.upgrade_firmwareless_devices()
 
-    @staticmethod
-    def dry_run(build):
-        related_device_fw = build._find_related_device_firmwares(select_devices=True)
-        firmwareless_devices = build._find_firmwareless_devices()
-        return {
-            "device_firmwares": related_device_fw,
-            "devices": firmwareless_devices,
-        }
-
     def upgrade_related_devices(self):
-        """
-        upgrades all devices which have an
-        existing related DeviceFirmware
-        """
         device_firmwares = self.build._find_related_device_firmwares()
         for device_fw in device_firmwares:
-            image = self.build.firmwareimage_set.filter(
-                type=device_fw.image.type
-            ).first()
+            image = self.build.firmwareimage_set.filter(type=device_fw.image.type).first()
             if image:
                 device_fw.image = image
                 device_fw.full_clean()
                 device_fw.save(self, upgrade_options=self.upgrade_options)
 
     def upgrade_firmwareless_devices(self):
-        """
-        upgrades all devices which do not
-        have a related DeviceFirmware yet
-        (referred as "firmwareless")
-        """
-        # for each image, find related "firmwareless"
-        # devices and perform upgrade one by one
         for image in self.build.firmwareimage_set.all():
             devices = self.build._find_firmwareless_devices(image.boards)
             for device in devices:
@@ -542,47 +386,22 @@ class AbstractBatchUpgradeOperation(UpgradeOptionsMixin, TimeStampedEditableMode
         aborted = self.upgrade_operations.filter(status="aborted").count()
         return self.__get_rate(aborted)
 
-    @property
-    def upgrader_class(self):
-        return self._get_upgrader_class()
-
-    @property
-    def upgrader_schema(self):
-        return self._get_upgrader_schema()
-
-    def _get_upgrader_class(self, related_device_fw=None, firmwareless_devices=None):
-        if self.upgrade_operations:
-            return get_upgrader_class_for_device(self.upgrade_operations[0].device)
-        related_device_fw = (
-            related_device_fw
-            or self.build._find_related_device_firmwares(select_devices=True)
-        )
-        if related_device_fw:
-            return get_upgrader_class_for_device(related_device_fw.first().device)
-        firmwareless_devices = (
-            firmwareless_devices or self.build._find_firmwareless_devices()
-        )
-        if firmwareless_devices:
-            return get_upgrader_class_for_device(firmwareless_devices.first())
-
-    def _get_upgrader_schema(self, related_device_fw=None, firmwareless_devices=None):
-        upgrader_class = self._get_upgrader_class(
-            related_device_fw, firmwareless_devices
-        )
-        return getattr(upgrader_class, "SCHEMA", None)
-
     def __get_rate(self, number):
         result = Decimal(number) / Decimal(self.total_operations) * 100
         return round(result, 2)
 
 
 class AbstractUpgradeOperation(UpgradeOptionsMixin, TimeStampedEditableModel):
+    RETRYING = "retrying"
+
     STATUS_CHOICES = (
         ("in-progress", _("in progress")),
         ("success", _("success")),
         ("failed", _("failed")),
         ("aborted", _("aborted")),
+        (RETRYING, _("retrying")),
     )
+
     device = models.ForeignKey(
         swapper.get_model_name("config", "Device"), on_delete=models.CASCADE
     )
@@ -590,7 +409,7 @@ class AbstractUpgradeOperation(UpgradeOptionsMixin, TimeStampedEditableModel):
         get_model_name("FirmwareImage"), null=True, on_delete=models.SET_NULL
     )
     status = models.CharField(
-        max_length=12, choices=STATUS_CHOICES, default=STATUS_CHOICES[0][0]
+        max_length=12, choices=STATUS_CHOICES, default="in-progress"
     )
     log = models.TextField(blank=True)
     batch = models.ForeignKey(
@@ -602,6 +421,16 @@ class AbstractUpgradeOperation(UpgradeOptionsMixin, TimeStampedEditableModel):
 
     class Meta:
         abstract = True
+
+    def retry_upgrade(self):
+        if self.status not in ["failed", "aborted"]:
+            raise ValidationError(_("Only failed or aborted upgrades can be retried."))
+
+        self.status = self.RETRYING
+        self.log_line("Retrying upgrade operation...")
+        self.save()
+
+        transaction.on_commit(lambda: upgrade_firmware.delay(self.pk))
 
     def log_line(self, line, save=True):
         if self.log:
@@ -631,8 +460,7 @@ class AbstractUpgradeOperation(UpgradeOptionsMixin, TimeStampedEditableModel):
                 return
 
             log_template = (
-                "Failed to connect with device using {credentials}."
-                " Error: {failure_reason}"
+                "Failed to connect with device using {credentials}. Error: {failure_reason}"
             )
             for conn in self.device.deviceconnection_set.select_related("credentials"):
                 self.log_line(
@@ -645,81 +473,59 @@ class AbstractUpgradeOperation(UpgradeOptionsMixin, TimeStampedEditableModel):
             self._recoverable_failure_handler(
                 recoverable,
                 RecoverableFailure(
-                    (
-                        "Failed to establish connection with the device,"
-                        " tried all DeviceConnections"
-                    )
+                    "Failed to establish connection with the device, tried all DeviceConnections"
                 ),
             )
             self.save()
             return
+
         installed = False
-        # prevent multiple upgrade operations for
-        # the same device running at the same time
         qs = (
             load_model("UpgradeOperation")
             .objects.filter(device=self.device, status="in-progress")
             .exclude(pk=self.pk)
         )
-        if qs.count() > 0:
-            message = "Another upgrade operation is in progress, aborting..."
-            logger.warning(message)
-            self.log_line(message, save=False)
+        if qs.exists():
+            self.log_line("Another upgrade operation is in progress, aborting...", save=False)
             self.status = "aborted"
             self.save()
             return
+
         upgrader_class = get_upgrader_class_from_device_connection(conn)
         if not upgrader_class:
             return
         upgrader = upgrader_class(self, conn)
+
         try:
             upgrader.upgrade(self.image.file)
-        # this exception is raised when the checksum present in the device
-        # equals the checksum of the image we are trying to flash, which
-        # means the device was aleady flashed previously with the same image
         except UpgradeNotNeeded:
             self.status = "success"
             installed = True
-        # this exception is raised when the test of the image fails,
-        # meaning the image file is corrupted or not apt for flashing
         except UpgradeAborted:
             self.status = "aborted"
-        # raising this exception will cause celery to retry again
-        # the upgrade according to its configuration
         except RecoverableFailure as e:
             self._recoverable_failure_handler(recoverable, e)
-        # failure to reconnect to the device after reflashing or any
-        # other unexpected exception will flag the upgrade as failed
         except (Exception, ReconnectionFailed) as e:
             cause = str(e)
             self.log_line(cause)
             self.status = "failed"
-            # if the reconnection failed we'll add some more info
             if isinstance(e, ReconnectionFailed):
-                # update device connection info
                 conn.is_working = False
                 conn.failure_reason = cause
                 conn.last_attempt = timezone.now()
                 conn.save()
-                # even if the reconnection failed,
-                # the firmware image has been flashed
                 installed = True
-        # if no exception has been raised, the upgrade was successful
         else:
             installed = True
             self.status = "success"
+
         self.save()
-        # if the firmware has been successfully installed,
-        # or if it was already installed
-        # set `instaleld` to `True` on the devicefirmware instance
         if installed:
             self.device.devicefirmware.installed = True
             self.device.devicefirmware.save(upgrade=False)
 
     def save(self, *args, **kwargs):
         result = super().save(*args, **kwargs)
-        # when an operation is completed
-        # trigger an update on the batch operation
         if self.batch and self.status != "in-progress":
             self.batch.update()
         return result
