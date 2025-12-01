72e9d2ccc660b265108e364e3868ad59a33c2cc2
Alexandre Vincent <alexandre.vincent@stellar.tc>
2025-12-01 15:39:15 +0100

    [change] Remove sample_config for now

--- tests/openwisp2/sample_config/apps.py
+++
@@ -1,9 +0,0 @@
-from openwisp_controller.config.apps import ConfigConfig
-
-
-class SampleConfigConfig(ConfigConfig):
-    name = "openwisp2.sample_config"
-    label = "sample_config"
-
-
-del ConfigConfig
