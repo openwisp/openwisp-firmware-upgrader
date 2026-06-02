9d3ad30bc4990aa5ab6434c0c062ce68ba294341
AlexV <asmodehn@gmail.com>
2026-06-02 10:52:42 +0200

    [change] Reactivate tests fixes for sample_connection

--- tests/openwisp2/sample_connection/tests.py
+++ tests/openwisp2/sample_connection/tests.py
@@ -1,3 +1,6 @@
+from django.urls import reverse
+
+from openwisp_controller.connection import settings as conn_settings
 from openwisp_controller.connection.tests.test_admin import (
     TestCommandInlines as BaseTestCommandInlines,
 )
@@ -50,29 +53,28 @@ class TestSsh(BaseTestSsh):
 
 class TestNotifications(BaseTestNotifications):
     app_label = "sample_connection"
-    # config_app_label = "config"
+    config_app_label = "config"
 
 
 class TestNotificationTransaction(BaseTestNotificationTransaction):
     app_label = "sample_connection"
-    # config_app_label = "config"
+    config_app_label = "config"
 
 
 class TestConnectionApi(BaseTestConnectionApi):
-    # def test_post_deviceconnection_list(self):
-    #     d1 = self._create_device()
-    #     self._create_config(device=d1)
-    #     path = reverse("connection_api:deviceconnection_list", args=(d1.pk,))
-    #     data = {
-    #         "credentials": self._get_credentials().pk,
-    #         "update_strategy": conn_settings.UPDATE_STRATEGIES[0][0],
-    #         "enabled": True,
-    #         "failure_reason": "",
-    #     }
-    #     with self.assertNumQueries(13):
-    #         response = self.client.post(path, data, content_type="application/json")
-    #     self.assertEqual(response.status_code, 201)
-    pass
+    def test_post_deviceconnection_list(self):
+        d1 = self._create_device()
+        self._create_config(device=d1)
+        path = reverse("connection_api:deviceconnection_list", args=(d1.pk,))
+        data = {
+            "credentials": self._get_credentials().pk,
+            "update_strategy": conn_settings.UPDATE_STRATEGIES[0][0],
+            "enabled": True,
+            "failure_reason": "",
+        }
+        with self.assertNumQueries(13):
+            response = self.client.post(path, data, content_type="application/json")
+        self.assertEqual(response.status_code, 201)
 
 
 del BaseTestCommandInlines
